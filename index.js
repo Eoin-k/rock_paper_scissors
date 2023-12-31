c297eaf5441d2f8ea90ce23263743f8d18a17dc4
Eoin-k <100050380+Eoin-k@users.noreply.github.com01~>
2023-12-31 02:48:10 +0000

    Updated Js logic for win / lose conditions

--- index.js
+++ index.js
@@ -15,27 +15,36 @@ let count = 0;
     computerSelection = getComputerChoice();
     if (playerSelection === "rock" && computerSelection === "paper"){
         computerWins++
+        count++
         let result = `You Lose ${computerSelection} beats ${playerSelection}`;
         return result;
     }
     if (playerSelection === "paper" && computerSelection === "scissors"){
         computerWins++
+        count++
         let result = `You Lose ${computerSelection} beats ${playerSelection}`;
         return result;
     }
     if (playerSelection === "scissors" && computerSelection === "rock"){
         computerWins++
+        count++
         let result = `You Lose ${computerSelection} beats ${playerSelection}`;
         return 
     }
     if (playerSelection === computerSelection){
-    
+    if (count != 0){
+        count--
         let result = `Its a draw ${computerSelection} and ${playerSelection} cancel each other out, we will replay this round`;
         return result;
+    } else {
+        let result = `Its a draw ${computerSelection} and ${playerSelection} cancel each other out, we will replay this round`;
+        return result;
+    }
         
     }
     else {
         playerWins++;
+        count++
         let result = `You win ${playerSelection} beats ${computerSelection}`;
     
         return result;
@@ -45,13 +54,16 @@ let count = 0;
  }
 
  function game(){
-    for (let count = 1; count<=5; count++){
+    for (let rounds = count ; rounds<=20; rounds++){
+        if(playerWins == 3 || computerWins == 3){
+            console.log( 'Game is over' )
+        } else {
         let message;
         playerSelection = prompt(message);
        console.log (playRound(playerSelection, computerSelection));
        console.log(count)
        }
-       
+    }
  }
 
 
